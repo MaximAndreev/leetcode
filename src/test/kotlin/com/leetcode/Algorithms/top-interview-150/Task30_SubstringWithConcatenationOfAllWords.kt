3ad6aab0ea911c13a9485307605154024823a058
Maxim Andreev <to.altor@gmail.com>
2025-06-23 18:34:52 +0300

    Add solution to the concatenation of all words

--- src/test/kotlin/com/leetcode/Algorithms/top-interview-150/Task30_SubstringWithConcatenationOfAllWords.kt
+++ src/test/kotlin/com/leetcode/Algorithms/top-interview-150/Task30_SubstringWithConcatenationOfAllWords.kt
@@ -19,7 +19,55 @@ import org.junit.jupiter.api.Test
 class Task30_SubstringWithConcatenationOfAllWords {
 
     fun findSubstring(s: String, words: Array<String>): List<Int> {
-        return emptyList()
+        if (words.isEmpty()) {
+            return emptyList()
+        }
+        val wordLength = words[0].length
+        if (wordLength == 0) {
+            return emptyList()
+        }
+        if (s.length < wordLength * words.size) {
+            return emptyList()
+        }
+
+        var counter = countWords(words)
+        var overallCount = words.size
+        var startPosition = 0
+        var currentPosition = 0
+        val answer: MutableList<Int> = mutableListOf()
+        while (startPosition < s.length - wordLength * words.size + 1) {
+            val word = s.substring(currentPosition, currentPosition + wordLength)
+            val count = counter.get(word)
+            if (count == null || count == 0) {
+                startPosition++
+                currentPosition = startPosition
+                if (overallCount != words.size) {
+                    counter = countWords(words)
+                    overallCount = words.size
+                }
+                continue
+            }
+            counter.merge(word, 1, Int::minus)
+            overallCount--
+            if (overallCount == 0) {
+                answer.add(startPosition)
+                startPosition++
+                currentPosition = startPosition
+                counter = countWords(words)
+                overallCount = words.size
+            } else {
+                currentPosition += wordLength
+            }
+        }
+        return answer
+    }
+
+    fun countWords(words: Array<String>): MutableMap<String, Int> {
+        val counter: MutableMap<String, Int> = mutableMapOf()
+        for (word in words) {
+            counter.merge(word, 1, Int::plus)
+        }
+        return counter
     }
 
     @Test
@@ -51,6 +99,50 @@ class Task30_SubstringWithConcatenationOfAllWords {
             findSubstring(
                 "barfoofoobarthefoobarman",
                 arrayOf("bar", "foo", "the")
+            )
+        )
+    }
+
+    @Test
+    fun case4() {
+        assertEquals(
+            emptyList<Int>(),
+            findSubstring(
+                "",
+                arrayOf("bar", "foo", "the")
+            )
+        )
+    }
+
+    @Test
+    fun case5() {
+        assertEquals(
+            emptyList<Int>(),
+            findSubstring(
+                "barfoofoobarthefoobarman",
+                arrayOf("")
+            )
+        )
+    }
+
+    @Test
+    fun case6() {
+        assertEquals(
+            listOf(0, 1, 2),
+            findSubstring(
+                "aaa",
+                arrayOf("a")
+            )
+        )
+    }
+
+    @Test
+    fun case7() {
+        assertEquals(
+            emptyList<Int>(),
+            findSubstring(
+                "aaa",
+                arrayOf("word")
             )
         )
     }
