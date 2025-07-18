ddd53e345c34d9242f71d0cfc3d8cff6fddb2abc
Maxim Andreev <to.altor@gmail.com>
2025-07-18 17:06:14 +0300

    Add valid sudoku task

---
+++ src/test/kotlin/com/leetcode/Algorithms/top-interview-150/Task36_ValidSudoku.kt
@@ -0,0 +1,55 @@
+package com.leetcode.Algorithms.`top-interview-150`
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+/**
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * Note:
+ *  - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ *  - Only the filled cells need to be validated according to the mentioned rules.
+ */
+class Task36_ValidSudoku {
+
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        return false
+    }
+
+    @Test
+    fun case1() {
+        val board: Array<CharArray> = arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+        assertTrue(isValidSudoku(board))
+    }
+
+    @Test
+    fun case2() {
+        val board: Array<CharArray> = arrayOf(
+            charArrayOf('8', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+        assertFalse(isValidSudoku(board))
+    }
+}
