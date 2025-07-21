76f47238aa2042b840a5d73b87a52c9331f5b291
Maxim Andreev <to.altor@gmail.com>
2025-07-21 11:57:17 +0300

    Add a solution to valid sudoku task

--- src/test/kotlin/com/leetcode/Algorithms/top-interview-150/Task36_ValidSudoku.kt
+++ src/test/kotlin/com/leetcode/Algorithms/top-interview-150/Task36_ValidSudoku.kt
@@ -1,6 +1,5 @@
 package com.leetcode.Algorithms.`top-interview-150`
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -18,7 +17,51 @@ import org.junit.jupiter.api.Test
 class Task36_ValidSudoku {
 
     fun isValidSudoku(board: Array<CharArray>): Boolean {
-        return false
+
+        // Walking rows
+        for (row in board.indices) {
+            val metNumbers = mutableSetOf<Char>()
+            for (col in board[0].indices) {
+                if (board[row][col] != '.' && metNumbers.contains(board[row][col])) {
+                    return false
+                } else {
+                    metNumbers.add(board[row][col])
+                }
+            }
+        }
+
+        // Walking columns
+        for (col in board[0].indices) {
+            val metNumbers = mutableSetOf<Char>()
+            for (row in board.indices) {
+                if (board[row][col] != '.' && metNumbers.contains(board[row][col])) {
+                    return false
+                } else {
+                    metNumbers.add(board[row][col])
+                }
+            }
+        }
+
+        val subBoxes = mutableMapOf<Int, MutableSet<Char>>()
+        for (j in 0..<3) {
+            for (i in 0..<3) {
+                subBoxes[i + j * 3] = mutableSetOf()
+                for (col in 0..<3) {
+                    for (row in 0..<3) {
+                        val actualRow = row + j * 3
+                        val actualCol = col + i * 3
+                        if (board[actualRow][actualCol] != '.' && subBoxes[i+j*3]!!.contains(board[actualRow][actualCol])) {
+                            return false
+                        } else {
+                            subBoxes[i+j*3]!!.add(board[actualRow][actualCol])
+                        }
+
+                    }
+                }
+            }
+        }
+
+        return true
     }
 
     @Test
@@ -49,6 +92,22 @@ class Task36_ValidSudoku {
             charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
             charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
             charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+        assertFalse(isValidSudoku(board))
+    }
+
+    @Test
+    fun case3() {
+        val board: Array<CharArray> = arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '1', '8', '.', '.', '7', '9')
         )
         assertFalse(isValidSudoku(board))
     }
